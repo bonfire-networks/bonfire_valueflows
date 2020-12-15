0c886257e717c01f774b7481c84744703ca6a683
Mayel <git@mayel.space>
2020-12-15 12:41:41 +0100

    add all migrations

---
+++ lib/all_migrations.ex
@@ -0,0 +1,39 @@
+defmodule ValueFlows.AllMigrations do
+  def up do
+    ValueFlows.Planning.Intent.Migrations.up()
+
+    ValueFlows.Proposal.Migrations.up()
+
+    ValueFlows.Knowledge.ResourceSpecification.Migrations.up()
+    ValueFlows.Knowledge.ProcessSpecification.Migrations.up()
+
+    ValueFlows.Observation.EconomicResource.Migrations.up()
+    ValueFlows.Observation.Process.Migrations.up()
+    ValueFlows.Observation.EconomicEvent.Migrations.up()
+
+    ValueFlows.Planning.Intent.Migrations.add_references()
+
+    ValueFlows.Claim.Migrations.up()
+
+    ValueFlows.ValueCalculation.Migrations.up()
+  end
+
+  def down do
+    ValueFlows.Planning.Intent.Migrations.down()
+
+    ValueFlows.Proposal.Migrations.down()
+
+    ValueFlows.Knowledge.ResourceSpecification.Migrations.down()
+    ValueFlows.Knowledge.ProcessSpecification.Migrations.down()
+
+    ValueFlows.Observation.EconomicResource.Migrations.down()
+    ValueFlows.Observation.Process.Migrations.down()
+    ValueFlows.Observation.EconomicEvent.Migrations.down()
+
+    ValueFlows.Planning.Intent.Migrations.add_references()
+
+    ValueFlows.Claim.Migrations.down()
+
+    ValueFlows.ValueCalculation.Migrations.down()
+  end
+end
