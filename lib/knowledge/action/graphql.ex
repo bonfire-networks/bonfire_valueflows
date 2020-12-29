679978b60db31c88c958f54adb3185ccd1e500a2
Mayel <git@mayel.space>
2020-12-29 12:28:28 +0100

    tweaks

--- lib/knowledge/action/graphql.ex
+++ lib/knowledge/action/graphql.ex
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: AGPL-3.0-only
-if Bonfire.Common.Utils.module_exists?(Bonfire.GraphQL) do
+if Code.ensure_loaded?(Bonfire.GraphQL) do
 defmodule ValueFlows.Knowledge.Action.GraphQL do
 
 
