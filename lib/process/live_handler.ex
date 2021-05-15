d7e8d92bbe079a4dc4148d09eeff7aadf5878990
Mayel <git@mayel.space>
2021-05-15 19:06:57 +0200

    edit process

--- lib/process/live_handler.ex
+++ lib/process/live_handler.ex
@@ -21,4 +21,30 @@ defmodule ValueFlows.Process.LiveHandler do
     end
   end
 
+  def handle_event("update", attrs, %{assigns: %{process: %{id: process_id}}} = socket) do
+    do_update(process_id, attrs |> Map.get("process") |> input_to_atoms(), socket)
+  end
+
+  def handle_event("status:finished", %{"id" => id} = attrs, socket) do
+    # TODO: record by who
+    do_update(id, %{finished: true}, socket)
+  end
+
+  def do_update(id, attrs, %{assigns: %{current_user: current_user}} = socket) do
+    IO.inspect(attrs: attrs)
+    # TODO: check permissions
+    with {:ok, process} <- Processes.one(id: id),
+         {:ok, process} <- Processes.update(process, attrs) do
+      # IO.inspect(intent)
+
+      redir = if e(attrs, "redirect_after", nil) do
+          e(attrs, "redirect_after", "/process/")<>process.id
+         else
+          e(socket.assigns, :current_url, "#")
+         end
+
+      {:noreply, socket |> push_redirect(to: redir) }
+    end
+  end
+
 end
