4a8e8fb38f76010880f05059ab4ac8ea1f8f6afe
Mayel <git@mayel.space>
2021-07-08 16:10:59 +0200

    error handling

--- lib/economic_event/live_handler.ex
+++ lib/economic_event/live_handler.ex
@@ -10,6 +10,7 @@ defmodule ValueFlows.EconomicEvent.LiveHandler do
 
   def handle_event("create", attrs, socket) do
     creator = current_user(socket) #|> IO.inspect(label: "creator")
+    # IO.inspect(socket: socket)
 
     with obj_attrs <- attrs
                       |> IO.inspect()
@@ -21,7 +22,7 @@ defmodule ValueFlows.EconomicEvent.LiveHandler do
                       |> IO.inspect(label: "create_event_attrs"),
     %{valid?: true} = cs <- changeset(obj_attrs),
     {:ok, event} <- EconomicEvents.create(creator, obj_attrs) do
-      IO.inspect(created: event)
+      # IO.inspect(created: event)
 
       if e(event, :economic_resource, :id, nil) do
         {:noreply, socket |> push_redirect(to: e(attrs, "redirect_after", "/resource/")<>e(event, :economic_resource, :id, ""))}
@@ -29,10 +30,14 @@ defmodule ValueFlows.EconomicEvent.LiveHandler do
         # {:noreply, socket |> push_redirect(to: e(attrs, "redirect_after", "/event/")<>e(event, :economic_event, :id, ""))}
         {:noreply, socket |> put_flash(:success, "Event recorded!")}
       end
+    # else
+    #   {:error, error} ->
+    #     {:noreply, assign(socket, form_error: error_msg(error))}
 
+    #   %Ecto.Changeset{} = cs ->
+    #     {:noreply, assign(socket, changeset: cs, form_error: error_msg(cs))} #|> IO.inspect
     end
   end
-
 
 
 end
