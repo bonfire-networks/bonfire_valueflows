df61339e636599f9e64796a9989e75ecd083fa5e
Mayel de Borniol <git@mayel.space>
2024-05-05 17:02:51 +0100

    readme

--- lib/value_flows.ex
+++ lib/value_flows.ex
@@ -1,7 +1,3 @@
 defmodule ValueFlows do
-  @moduledoc """
-  [ValueFlows](https://www.valueflo.ws) implemented as a Bonfire extension.
-
-  Refer to the [README](https://doc.bonfirenetworks.org/value_flows.html)
-  """
+  @moduledoc "./README.md" |> File.stream!() |> Enum.drop(1) |> Enum.join()
 end
