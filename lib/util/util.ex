d2e64d6c960b8123f1a37dfb9acee7b5e9d20734
Mayel de Borniol <git@mayel.space>
2024-11-24 18:26:02 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/1014

--- lib/util/util.ex
+++ lib/util/util.ex
@@ -195,7 +195,8 @@ defmodule ValueFlows.Util do
     repo().maybe_preload(obj,
       creator: [
         :character,
-        profile: [:icon]
+        :profile
+        # : [:icon]
       ]
     )
     |> Map.get(:creator)
@@ -207,10 +208,7 @@ defmodule ValueFlows.Util do
   end
 
   def indexing_format_creator(%{id: id} = creator) when not is_nil(id) do
-    Bonfire.Me.Integration.indexing_format_creator(
-      maybe_get(creator, :profile),
-      maybe_get(creator, :character)
-    )
+    Bonfire.Me.Integration.indexing_format_creator(creator)
   end
 
   def indexing_format_creator(_) do
