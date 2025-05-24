5fd1a62308635c9c98d2608694ead591c9a92111
Mayel de Borniol <git@mayel.space>
2025-05-24 13:56:29 +0100

    otp27

--- mess.exs
+++ mess.exs
@@ -1,4 +1,5 @@
-# Copyright (c) 2020 James Laver, mess Contributors
+# Copyright (c) 2020 James Laver 
+# Copyright (c) 2025 Bonfire contributors
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -6,10 +7,10 @@
 if not Code.ensure_loaded?(Mess) do
   defmodule Mess do
     @sources [path: "deps.path", git: "deps.git", hex: "deps.hex"]
-
-    @newline ~r/(?:\r\n|[\r\n])/
-    @parser ~r/^(?<indent>\s*)((?<package>[a-z_][a-z0-9_]+)\s*=\s*"(?<value>[^"]+)")?(?<post>.*)/
-    @git_branch ~r/(?<repo>[^#]+)(#(?<branch>.+))?/
+    
+    defp newline, do: ~r/(?:\r\n|[\r\n])/
+    defp parser, do: ~r/^(?<indent>\s*)((?<package>[a-z_][a-z0-9_]+)\s*=\s*"(?<value>[^"]+)")?(?<post>.*)/
+    defp git_branch, do: ~r/(?<repo>[^#]+)(#(?<branch>.+))?/
 
     def deps(sources \\ @sources, deps),
       do: deps(Enum.flat_map(sources, fn {k, v} -> read(v, k) end), deps, :deps)
@@ -24,10 +25,10 @@ if not Code.ensure_loaded?(Mess) do
     defp read({:error, :enoent}, _kind), do: []
 
     defp read({:ok, file}, kind),
-      do: Enum.map(String.split(file, @newline), &read_line(&1, kind))
+      do: Enum.map(String.split(file, newline()), &read_line(&1, kind))
 
     defp read_line(line, kind),
-      do: Map.put(Regex.named_captures(@parser, line), :kind, kind)
+      do: Map.put(Regex.named_captures(parser(), line), :kind, kind)
 
     defp dep_spec(%{"package" => ""}), do: []
 
@@ -40,7 +41,7 @@ if not Code.ensure_loaded?(Mess) do
     defp dep_spec(%{"package" => p, "value" => v, :kind => :git}), do: git(v, p)
 
     defp git(line, p) when is_binary(line),
-      do: git(Regex.named_captures(@git_branch, line), p)
+      do: git(Regex.named_captures(git_branch(), line), p)
 
     defp git(%{"branch" => "", "repo" => r}, p),
       do: pkg(p, git: r, override: true)
